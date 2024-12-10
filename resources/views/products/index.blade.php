cdca41b1d46fd95952bbde11e89f1531719aeae0
cahyani1212 <nichya401@gmail.com>
2024-12-10 22:28:00 +0800

    fix tampilan deskripsi product

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -86,7 +86,7 @@
                                     <td class="px-4 py-2">{{ $product->id }}</td>
                                     <td class="px-4 py-2">{{ $product->id_kategori }}</td>
                                     <td class="px-4 py-2">{{ $product->nama_produk}}</td>
-                                    <td class="px-4 py-2">{{ $product->description }}</td>
+                                    <td class="px-4 py-2">{{ $product->deskripsi }}</td>
                                     <td class="px-4 py-2">{{ $product->ukuran }}</td>
                                     <td class="px-4 py-2">{{ $product->warna }}</td>
                                     <td class="px-4 py-2">{{ $product->stok }}</td>
